87a12948c5d36d60e6f5de1450d9d08ad1d45446
Taxmalalas0001 <nikosstsairidiss@gmail.com>
2026-01-05 01:22:47 +0200

    Fix Syntax Errors

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
-  base : '/'
+  base : '/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
